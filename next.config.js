3ce3a3be6ddef1e44e016b8eddd4d25eb5d51bfe
alfiemitchell123 <alfiemitchell1@gmail.com>
2024-02-17 17:23:44 -0800

    added out folder

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   // Use the prefix in production and on GitHub Pages
   assetPrefix: process.env.NODE_ENV === 'production' ? '/{ISRC_Calculator}' : '',
+  output: 'export',
 };
 
 module.exports = nextConfig;
